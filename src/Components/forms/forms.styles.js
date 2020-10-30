27c090566af1833af66cd3789d5077899b017bf0
witalo <sousawitalo@gmail.com>
2020-10-30 12:44:14 -0300

    read-md adicionado

--- src/Components/forms/forms.styles.js
+++ src/Components/forms/forms.styles.js
@@ -1,35 +1,34 @@
 import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles((theme) => ({
-  container: {
-    minHeight: "800px",
-  },
-  paper: {
-    marginTop: theme.spacing(8),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main,
-  },
-  form: {
-    width: "100%", // Fix IE 11 issue.
-    marginTop: theme.spacing(3), 
- 
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2),
-  },
-  textFormArea: {
-    paddingRight: theme.spacing(1),
-    paddingLeft: theme.spacing(1),
-  },
-  gridContainer: {
-    display: 'flex',
-    justifyContent: 'center',
-  }
+    container: {
+        minHeight: "800px",
+    },
+    paper: {
+        marginTop: theme.spacing(8),
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: "100%", // Fix IE 11 issue.
+        marginTop: theme.spacing(3),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+    textFormArea: {
+        paddingRight: theme.spacing(1),
+        paddingLeft: theme.spacing(1),
+    },
+    gridContainer: {
+        display: "flex",
+        justifyContent: "center",
+    },
 }));
 
 export default useStyles;
--- src/Components/forms/index.js
+++ src/Components/forms/index.js
@@ -1,3 +1,6 @@
-export {default as LoginForm} from "./login.form";
-export {default as PasswordRecoverForm} from "./PasswordRecover.form";
-export {default as RegisterForm} from "./register.form";
+export { default as LoginForm }
+from "./login.form";
+export { default as PasswordRecoverForm }
+from "./PasswordRecover.form";
+export { default as RegisterForm }
+from "./register.form";
