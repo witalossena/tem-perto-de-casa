309a19269c0678f0e37a404b79b9dbc1724fded2
witalo <sousawitalo@gmail.com>
2020-11-04 17:22:17 -0300

    add changes

--- src/Services/api.js
+++ src/Services/api.js
@@ -1,9 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
     // baseURL: 'http://localhost:3333'
-    // baseURL: 'https://tpdc.herokuapp.com/'
-    baseURL: process.ENV.REACT_APP_API_URL
+    // baseURL: "https://tpdc.herokuapp.com/",
+    baseURL: process.env.REACT_APP_API_URL
 });
 
 export default api;
